e7cd28f40ff615cd18966206921e41907ffa496e
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-05-04 19:49:40 +0200

    Testing markdown and html output

--- analysis_scripts/orthoptera_elevation_data_exploration.R
+++ analysis_scripts/orthoptera_elevation_data_exploration.R
@@ -18,10 +18,16 @@ latest_commit <- system("git show -s --pretty='%h on %ci' HEAD", intern=TRUE)
 system("pwd")
 
 
-this_file_latest_commits <- system("git log --pretty='tformat:%h <<%s>> on %ci<br />' orthoptera_elevation_data_exploration.R  | tr -d '\n'", intern=TRUE)
+this_file_latest_commits <- system("git log --pretty='tformat:- %h %s on %ci' analysis_scripts/orthoptera_elevation_data_exploration.R", intern=TRUE)
+#this_file_latest_commits <- cat("<ul>", this_file_latest_commits, "</ul>")
 
 #' ## Change log
 {{ this_file_latest_commits }}
+
+test_carles <- paste("Hello <b>hello</b> this is *markdown*\n* line1\n* line2\n* line3\n")
+
+#' This is a Carles test
+{{ test_carles }}
 
 #' Test output as HTML
 
