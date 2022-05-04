ae3ea9a98781897ea26c84c40f5eb0390511d614
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-05-04 13:16:37 +0200

    Add new line to changelog output. Improve comments and restructure.

--- analysis_scripts/orthoptera_elevation_data_exploration.R
+++ analysis_scripts/orthoptera_elevation_data_exploration.R
@@ -1,6 +1,5 @@
 #' ---
 #' title: Orthoptera species richness, elevational range and community composition
-#' subtitle: Data exploration
 #' output: 
 #'   html_document:
 #'     toc: false
@@ -9,19 +8,26 @@
 #' ---
 
 # Latest Git commits and updates
-system("ls")
-setwd <- 'orthoptera-elevational-range-community-composition'
+
 latest_commit <- system("git show -s --pretty='%h on %ci' HEAD", intern=TRUE)
 
 #' The latest change to this project was **commit **
 {{ latest_commit }}
 
-this_file_latest_commits <- system("git log --pretty='tformat:%h <<%s>> on %ci %n' orthoptera_elevation_data_exploration.R", intern=TRUE)
+this_file_latest_commits <- system("git log --pretty='tformat:%h <<%s>> on %ci<br>' orthoptera_elevation_data_exploration.R", intern=TRUE)
 
-#' The most recent changes to this file were
+#' ## Change log
 {{ this_file_latest_commits }}
 # TODO improve the formatting of this output
 
 #' Test output as HTML
 
 print('hello')
+
+# Project set-up
+
+setwd <- 'orthoptera-elevational-range-community-composition' # relative path
+
+# Read in data files
+
+
