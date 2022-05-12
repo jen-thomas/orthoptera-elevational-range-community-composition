2b90005e179c2503692db730ee0e62ee0859a1dd
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-05-12 11:28:28 +0200

    Create aggregated dataframe to be able to build species-site matrix.
    Update renv.lock file with newer version of rlang.

--- analysis_scripts/orthoptera_elevation_data_exploration.R
+++ analysis_scripts/orthoptera_elevation_data_exploration.R
@@ -6,7 +6,7 @@
 #'     theme: yeti
 #'     code_folding: hide
 #' ---
-setwd("analysis_scripts/")
+
 # Latest Git commits and updates
 
 latest_commit <- system("git show -s --pretty='%h on %ci' HEAD", intern=TRUE)
@@ -18,14 +18,32 @@ this_file_latest_commits <- system("git log --pretty='tformat:- %h %s on %ci' or
 this_file_latest_commits <- paste("<ul>", this_file_latest_commits, "</ul>")
 
 #' ## Change log
+
 {{ this_file_latest_commits }}
 
+#'
+#' ## Set up
+
 # Read in data files
-getwd()
+
 observations <- read.csv("../data/observations.csv", header = TRUE, stringsAsFactors = TRUE)
 sites <- read.csv("../data/sites.csv", header = TRUE, stringsAsFactors = TRUE)
 surveys <- read.csv("../data/surveys.csv", header = TRUE, stringsAsFactors = TRUE)
 vegetation_plots <- read.csv("../data/vegetation_plots.csv", header = TRUE, stringsAsFactors = TRUE)
+
+#' ## Create species-site matrix
+#' The species-site matrix with abundances will be created using the observation data. 
+
+# Aggregate the observations of each species at each site (ignore dates for now).
+
+# Create vector of 1s to act as presence, join the vector with the observations dataframe, then sum over these to calculate abundance.
+presence <- rep(1, nrow(observations))
+
+observations$presence <- presence
+site_species_abundance <- setNames(aggregate(observations$presence, list(observations$site_name, observations$species), FUN=sum),
+                                   c("site_name", "species", "abundance"))
+
+
 
 
 
