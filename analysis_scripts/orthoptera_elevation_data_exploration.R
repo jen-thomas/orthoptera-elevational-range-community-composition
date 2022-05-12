511135f4209e1e9603b0659251e877b2347c95e8
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-05-12 16:10:23 +0200

    Put package install into function. Rename intermediate variables used in creation of species-site matrices. Improve comments.

--- analysis_scripts/orthoptera_elevation_data_exploration.R
+++ analysis_scripts/orthoptera_elevation_data_exploration.R
@@ -9,14 +9,16 @@
 
 # Set up
 
-install.packages("fossil", quiet = TRUE)
-install.packages("stringr", quiet = TRUE)
-library(fossil, quietly = TRUE)
-library(stringr, quietly = TRUE)
+vector_packages <- c("fossil", "stringr")
+
+install_packages <- function(vector_packages) {
+  for (required_package in vector_packages) {
+    if(!require(required_package)) install.packages(required_package, quiet=TRUE)}
+}
 
 #' ## Change log
 
-# Latest Git commits
+# Get the latest Git commits to this file
 
 this_file_latest_commits <- system("git log HEAD --pretty='tformat: %ci commit %h: %s' orthoptera_elevation_data_exploration.R", intern=TRUE)
 this_file_latest_commits_formatted <- paste(this_file_latest_commits, "<br>")
@@ -32,15 +34,22 @@ surveys <- read.csv("../data/surveys.csv", header = TRUE, stringsAsFactors = TRU
 vegetation_plots <- read.csv("../data/vegetation_plots.csv", header = TRUE, stringsAsFactors = TRUE)
 
 #' ## Create species-site matrix
-#' The species-site matrix with abundances will be created using the observation data. 
 
-# Aggregate the observations of each species at each site (ignore dates for now).
+# Add presence to the observation data frame.
 
 presence <- rep(1, nrow(observations)) # create vector of 1s to act as presence
-observations$presence <- presence # join the vector with the observations dataframe
+observations$presence <- presence # join the vector with the observations data frame
 
-site_species_abundance <- setNames(aggregate(observations$presence, list(observations$site_name, observations$species), FUN=sum),
-                                   c("site_name", "species", "abundance")) # sum over these to calculate abundance of each species at each site
+# Combine site_name and altitude columns to get site names that are more useful when looking at the data.
 
-site_species_abundance_matrix <- t(create.matrix(site_species_abundance, tax.name="species", locality="site_name", abund=TRUE, abund.col="abundance")) # transform it to have species names as columns and sites as rows
-site_species_presenceabsence_matrix <- t(create.matrix(site_species_abundance, tax.name="species", locality="site_name", abund=FALSE, abund.col="abundance")) # transform it to have species names as columns and sites as rows
+observations_presence <- (merge(x = observations, y = sites, by = "site_name", all.x = TRUE))[, c('site_name', 'altitude_band_m', 'suborder', 'family', 'subfamily', 'genus', 'species', 'presence')]
+observations_presence$site_altitude <- paste(observations_presence$site_name, observations_presence$altitude_band_m, sep="_")
+
+# Aggregate the observations of each species at each site (ignore dates for now). Then create the species-site abundance and presence-absence matrices. Transform the matrices to have species names as column names and sites as row names.
+
+site_species_abundance <- setNames(aggregate(observations_presence$presence, list(observations_presence$site_altitude, observations_presence$species), FUN=sum),
+                                   c("site_altitude", "species", "abundance")) # sum over these to calculate abundance of each species at each site
+
+site_species_abundance_matrix <- t(create.matrix(site_species_abundance, tax.name="species", locality="site_altitude", abund=TRUE, abund.col="abundance")) # transform it to have species names as columns and sites as rows
+site_species_presenceabsence_matrix <- t(create.matrix(site_species_abundance, tax.name="species", locality="site_altitude", abund=FALSE, abund.col="abundance")) # transform it to have species names as columns and sites as rows
+
