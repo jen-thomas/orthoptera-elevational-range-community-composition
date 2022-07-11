ad5a9dfd3d94720f5e6c86d6727eeda2c62c2ff1
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-07-11 11:14:38 +0100

    Reorganise structure.

--- analysis_scripts/orthoptera_elevational_range_hypothesis2.R
+++ analysis_scripts/orthoptera_elevational_range_hypothesis2.R
@@ -19,20 +19,16 @@ source("orthoptera_elevation_data_exploration.R")
 vector_packages <- c("visreg", "ggplot2", "dplyr")
 get_packages(vector_packages)
 
-#' ## Create site-species matrix.
+#' ## Prepare data.
 #+ message=FALSE, warning=FALSE
-
-#' Create a dataframe of species abundance at each site (note that abundance is not really relevant in
-#' this study because multiple capture techniques were used). Convert this into a presence-absence
-#' site-species matrix.
 #'
-#' The following functions are used to create the species-site matrix.
+#' The following functions are used to prepare the data and create the species-site matrix.
 
 create_site_species_abundance_df <- function(observations_df) {
-    #' Create a data frame of the species abundance at each site from a set of observations at different
-    #' sites. Each input observation is of one individual at a particular site.
-    #'
-    #' Return species abundance at each site.
+      #' Create a data frame of the species abundance at each site from a set of observations at different
+      #' sites. Each input observation is of one individual at a particular site.
+      #'
+      #' Return species abundance at each site.
 
   # Add presence to the observation data frame.
   presence <- rep(1, nrow(observations_df)) # create vector of 1s to act as presence
@@ -44,14 +40,14 @@ create_site_species_abundance_df <- function(observations_df) {
                                                     observations_df$species),
                                                FUN = sum), # sum over these to calculate abundance of
                                      # each species at each site
-                                               c("site_elevation", "species", "abundance"))
+                                     c("site_elevation", "species", "abundance"))
 
   return(site_species_abundance)
 }
 
 create_presence_absence_site_species_matrix <- function(observations_df) {
-    #' Create a presence-absence site-species matrix. Each value is either 0 (species not observed at a
-    #' site) or 1 (species observed at a site).
+      #' Convert the abundance matrix into a presence-absence site-species matrix. Each value is either 0
+      #' (species not observed at a site) or 1 (species observed at a site).
 
   site_species_abundance <- create_site_species_abundance_df(observations_df)
   site_species_presenceabsence_matrix <- t(create.matrix(site_species_abundance, tax.name = "species",
@@ -61,8 +57,6 @@ create_presence_absence_site_species_matrix <- function(observations_df) {
   return(site_species_presenceabsence_matrix)
 }
 
-#'Create and preview the presence-absence site-species matrix. Site name is in the format altitude(m)_site
-#' where the name is an abbreviation of the study area.
 
 observations_file <- "../data/observations.csv"
 sites_file <- "../data/sites.csv"
@@ -102,17 +96,20 @@ observations_species_sites
 #' or more sites.
 #'
 #' Three sites:
-species_three_or_more_sites <- observations_species_sites[(observations_species_sites$number_sites >= 3), ]
+species_three_or_more_sites <- observations_species_sites[(observations_species_sites$number_sites >= 3),]
 species_three_or_more_sites
 
 #' Five sites:
-species_five_or_more_sites <- observations_species_sites[(observations_species_sites$number_sites >= 5), ]
+species_five_or_more_sites <- observations_species_sites[(observations_species_sites$number_sites >= 5),]
 species_five_or_more_sites
 
 species_for_analysis <- unique(species_three_or_more_sites["species"])
 observations_to_use <- get_observations_of_particular_species(observations_species, species_for_analysis)
 
-#' Create the site-species matrix (presence-absence).
+#' ## Create site-species matrix.
+
+#' #'Create and preview the presence-absence site-species matrix. Site name is in the format altitude(m)_site
+#' where the name is an abbreviation of the study area.
 
 site_species_matrix <- create_presence_absence_site_species_matrix(observations_to_use)
 site_species_matrix
