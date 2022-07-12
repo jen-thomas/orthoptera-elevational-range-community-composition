d5dddea0cfd91d0a775adedf588bfb73e86db65f
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-07-12 09:49:11 +0100

    Compute quadratic, cubic and quartic. Model each of these and compare models.

--- analysis_scripts/orthoptera_elevational_range_hypothesis2.R
+++ analysis_scripts/orthoptera_elevational_range_hypothesis2.R
@@ -16,7 +16,7 @@ source("utils.R")
 source("data_preparation.R")
 source("orthoptera_elevation_data_exploration.R")
 
-vector_packages <- c("visreg", "ggplot2", "dplyr")
+vector_packages <- c("visreg", "ggplot2", "dplyr", "rcompanion")
 get_packages(vector_packages)
 
 #' ## Prepare data.
@@ -75,15 +75,15 @@ observations_to_use <- get_observations_of_particular_species(observations_speci
 #' The functions below calculate the elevational range of each taxa.
 
 calculate_elevational_range <- function(observations) {
-    #' Calculate the minimum, maximum and mid-point of the elevational range for each taxa. Assume here that
-    #' all specimens are identified to species.
+      #' Calculate the minimum, maximum and mid-point of the elevational range for each taxa. Assume here that
+      #' all specimens are identified to species.
 
   elevational_ranges_species <- observations %>%
     distinct(species, elevational_band_m) %>%
     group_by(species) %>%
     dplyr::summarise("min_elevation" = min(elevational_band_m), "max_elevation" = max(elevational_band_m),
                      "elevational_range" = max_elevation - min_elevation,
-                     "elevational_range_midpoint" = max_elevation - elevational_range/2,
+                     "elevational_range_midpoint" = max_elevation - elevational_range / 2,
                      "mean_elevation" = mean(elevational_band_m))
 
   return(elevational_ranges_species)
@@ -103,7 +103,7 @@ elevational_ranges_species <- calculate_elevational_range(observations_to_use)
 #'
 #' Look at the distribution of elevational range.
 
-par(mfrow = c(2,2))
+par(mfrow = c(2, 2))
 elevational_ranges_species$sqrt_elevational_range <- sqrt(elevational_ranges_species$elevational_range)
 elevational_ranges_species$log_elevational_range <- log(elevational_ranges_species$elevational_range)
 hist(elevational_ranges_species$elevational_range, xlab = "Elevational range (m a.s.l)", ylab = "Frequency")
@@ -116,17 +116,17 @@ hist(elevational_ranges_species$log_elevational_range, xlab = "Log elevational r
 #' species was observed.
 
 plot(elevational_range ~ elevational_range_midpoint, data = elevational_ranges_species,
-       xlab = "Elevational range midpoint (m a.s.l)", ylab = "Elevational range (m a.s.l)")
+     xlab = "Elevational range midpoint (m a.s.l)", ylab = "Elevational range (m a.s.l)")
 
 plot(elevational_range ~ mean_elevation, data = elevational_ranges_species,
-       xlab = "Mean elevation (m a.s.l)", ylab = "Elevational range (m a.s.l)")
+     xlab = "Mean elevation (m a.s.l)", ylab = "Elevational range (m a.s.l)")
 
 #' From these plots, we can see that the relationship does not appear to be linear.
 
 lin_reg_elevational_range_midpoint <- lm(elevational_ranges_species[["elevational_range"]] ~
                                            elevational_ranges_species[["elevational_range_midpoint"]])
 summary(lin_reg_elevational_range_midpoint)
-par(mfrow = c(2,2))
+par(mfrow = c(2, 2))
 plot(lin_reg_elevational_range_midpoint)
 
 #' Plot then test the relationship between the mean elevation and elevational range at which each species was observed.
@@ -134,9 +134,36 @@ plot(lin_reg_elevational_range_midpoint)
 lin_reg_mean_elevation <- lm(elevational_ranges_species[["elevational_range"]] ~
                                elevational_ranges_species[["mean_elevation"]])
 summary(lin_reg_mean_elevation)
-par(mfrow = c(2,2))
+par(mfrow = c(2, 2))
 plot(lin_reg_mean_elevation)
 
 #' There is a clear non-linear relationship between the elevational range and both measures of elevation
 #' (mean and mid-point) as seen on both of the plots. The linear regression shows the linear relationship
 #' is not statistically significant.
+#'
+#' The relationship seems to be quadratic. Test the quadratic, cubic and quartic models. Calculate these
+#' parameters first.
+
+elevational_ranges_species$mean_elevation2 <- (elevational_ranges_species$mean_elevation)^2
+elevational_ranges_species$mean_elevation3 <- (elevational_ranges_species$mean_elevation)^3
+elevational_ranges_species$mean_elevation4 <- (elevational_ranges_species$mean_elevation)^4
+
+nonlin_reg_quadratic <- lm(elevational_range ~ mean_elevation + mean_elevation2,
+                        data = elevational_ranges_species)
+nonlin_reg_cubic <- lm(elevational_range ~ mean_elevation + mean_elevation2 + mean_elevation3,
+                       data = elevational_ranges_species)
+nonlin_reg_quartic <- lm(elevational_range ~ mean_elevation + mean_elevation2 + mean_elevation3 +
+                         mean_elevation4, data = elevational_ranges_species)
+
+#' Compare the models to look at the AIC and adjusted R squared.
+
+compareLM(lin_reg_mean_elevation, nonlin_reg_quadratic, nonlin_reg_cubic, nonlin_reg_quartic)
+
+#' Do a direct comparison of the difference between the models using ANOVA.
+#' <br>H<sub>0</sub>: there is no difference between the models.
+#' <br>H<sub>1</sub>: there is a difference between the models.
+#+ message=FALSE, warning=FALSE
+
+anova(lin_reg_mean_elevation, nonlin_reg_quadratic, nonlin_reg_cubic, nonlin_reg_quartic)
+
+summary(nonlin_reg_quadratic)
