65ab3c91e75882fe6e73527457f4ab61edd249db
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-07-13 16:02:54 +0100

    Add comment.

--- analysis_scripts/orthoptera_community_composition_hypothesis3.R
+++ analysis_scripts/orthoptera_community_composition_hypothesis3.R
@@ -56,8 +56,15 @@ all_observations_conservative <- get_conservative_observations(confirmed_observa
 
 unique_taxa_sites <- get_unique_taxa_site(all_observations_conservative)
 unique_taxa_sites
+
 #' Create and preview the presence-absence site-species matrix. Site name is in the format
 #' altitude(m)_site where the name is an abbreviation of the study area.
+#'
+#' This matrix includes all unique taxa from each site (as was used for the species richness analysis).
+#' This means though, that a higher taxonomic level, such as genus, may be represented elsewhere in the
+#' matrix by a different taxa. The genus though in this example, would be a unique taxa from the
+#' particular sites where it was observed. **TODO**: consider if these should be removed from the analysis
+#' if they are not a unique taxa for the whole project, rather than just for a site.
 
 site_species_matrix <- create_presence_absence_site_species_matrix(unique_taxa_sites)
 site_species_matrix
