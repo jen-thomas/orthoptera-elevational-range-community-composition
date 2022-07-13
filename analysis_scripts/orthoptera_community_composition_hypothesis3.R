16aaa7b6d6ba6c2bea63b1bd1ceeacfab0a4b6aa
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-07-13 16:00:26 +0100

    Remove function which calculates abundance of each species at each site. Not needed.

--- analysis_scripts/orthoptera_community_composition_hypothesis3.R
+++ analysis_scripts/orthoptera_community_composition_hypothesis3.R
@@ -23,28 +23,7 @@ get_packages(vector_packages)
 
 #' ## Create site-species matrix.
 
-#' The following functions are used to prepare the data and create the species-site matrix.
-
-create_site_species_abundance_df <- function(observations_df) {
-        #' Create a data frame of the species abundance at each site from a set of observations at
-        #' different sites. Each input observation is of one individual at a particular site.
-        #'
-        #' Return species abundance at each site.
-
-  # Add presence to the observation data frame.
-  presence <- rep(1, nrow(observations_df)) # create vector of 1s to act as presence
-  observations_df$presence <- presence # join the vector with the observations data frame
-
-  # Aggregate the observations of each species at each site (ignore dates for now).
-  site_species_abundance <- setNames(aggregate(observations_df$presence,
-                                               list(observations_df$site_elevation,
-                                                    observations_df$species),
-                                               FUN = sum), # sum over these to calculate abundance of
-                                     # each species at each site
-                                     c("site_elevation", "species", "abundance"))
-
-  return(site_species_abundance)
-}
+#' The following function is used to prepare the data and create the species-site matrix.
 
 create_presence_absence_site_species_matrix <- function(observations_df) {
         #' Convert the abundance matrix into a presence-absence site-species matrix. Each value is either
