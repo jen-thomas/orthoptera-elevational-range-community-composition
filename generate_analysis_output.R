7849f9981ea206bb24c097ceb14674fb7f2b9bc5
Jen Thomas <jenny_t152@yahoo.co.uk>
2022-07-19 17:05:50 +0100

    Uncomment code to generate HTML output.

--- generate_analysis_output.R
+++ generate_analysis_output.R
@@ -9,13 +9,13 @@ rmarkdown::render("analysis_scripts/prepare_vegetation_data.R",
                   output_dir = "analysis_outputs/")
 rmarkdown::render("analysis_scripts/get_physical_site_data.R",
                   output_dir = "analysis_outputs/")
-# rmarkdown::render("analysis_scripts/get_finalised_observations_species_richness_conservative.R",
-#                   output_dir = "analysis_outputs/")
-# rmarkdown::render("analysis_scripts/orthoptera_elevation_data_exploration.R",
-#                   output_dir = "analysis_outputs/")
-# rmarkdown::render("analysis_scripts/orthoptera_species_richness_hypothesis1.R",
-#                   output_dir = "analysis_outputs/")
-# rmarkdown::render("analysis_scripts/orthoptera_elevational_range_hypothesis2.R",
-#                   output_dir = "analysis_outputs/")
+rmarkdown::render("analysis_scripts/get_finalised_observations_species_richness_conservative.R",
+                  output_dir = "analysis_outputs/")
+rmarkdown::render("analysis_scripts/orthoptera_elevation_data_exploration.R",
+                  output_dir = "analysis_outputs/")
+rmarkdown::render("analysis_scripts/orthoptera_species_richness_hypothesis1.R",
+                  output_dir = "analysis_outputs/")
+rmarkdown::render("analysis_scripts/orthoptera_elevational_range_hypothesis2.R",
+                  output_dir = "analysis_outputs/")
 rmarkdown::render("analysis_scripts/orthoptera_community_composition_hypothesis3.R",
                   output_dir = "analysis_outputs/")
