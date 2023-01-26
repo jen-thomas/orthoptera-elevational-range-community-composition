c8871d98d31dbfaf76ba9e5069f4a1c68b43b701
Jen Thomas <jenny_t152@yahoo.co.uk>
2023-01-26 10:21:50 +0100

    Select linear model (best fitting). Refactor code to use this for the plot.

--- analysis_scripts/test_claridge_singhrao_1978_data.R
+++ analysis_scripts/test_claridge_singhrao_1978_data.R
@@ -25,22 +25,37 @@ get_packages(vector_packages)
 elevational_ranges_species <- "../data/elevational_observations_claridge_singhrao_1978.csv"
 elevational_ranges_species_df <- read_csv_data_file(elevational_ranges_species)
 
-#' ### Calculate polynomials
+#' ## Create the linear models
+
 elevational_ranges_species_df <- calculate_polynomials_elevation(elevational_ranges_species_df)
+
+#' Test the linear, quadratic, cubic and quartic models. Calculate these parameters first to use in the 
+#' models.
 
 lin_regs_polynomial <- linear_regression_elevrange_elevation_polynomial(elevational_ranges_species_df)
 
-lin_reg_ <- lin_regs_polynomial[[1]]
+lin_reg <- lin_regs_polynomial[[1]]
 nonlin_reg_quadratic <- lin_regs_polynomial[[2]]
 nonlin_reg_cubic <- lin_regs_polynomial[[3]]
 nonlin_reg_quartic <- lin_regs_polynomial[[4]]
 
-#' ### Compare the polynomial models
-compareLM(lin_reg_, nonlin_reg_quadratic, nonlin_reg_cubic, nonlin_reg_quartic)
+#' Compare the linear models
+compareLM(lin_reg, nonlin_reg_quadratic, nonlin_reg_cubic, nonlin_reg_quartic)
 
-anova(lin_reg_, nonlin_reg_quadratic, nonlin_reg_cubic, nonlin_reg_quartic)
+#' We can see that the linear model has the lowest AIC, suggesting it is the best model for these data.
+#' This model also has the largest adjusted R-squared.
+#'
+#' Do a direct comparison of the difference between the models using ANOVA.
+#' <br>H<sub>0</sub>: there is no difference between the models.
+#' <br>H<sub>1</sub>: there is a difference between the models.
+#+ message=FALSE, warning=FALSE
 
-elevational_ranges_species_predicted <- cbind(elevational_ranges_species_df, predict(nonlin_reg_quadratic, interval = "confidence"))
+anova(lin_reg, nonlin_reg_quadratic, nonlin_reg_cubic, nonlin_reg_quartic)
+
+#' The comparison of the models shows that the polynomial models are not significantly better than the 
+#' linear model.
+
+elevational_ranges_species_predicted <- cbind(elevational_ranges_species_df, predict(lin_reg, interval = "confidence"))
 
 ### Prepare predicted and fitted values
 #' Get min and max of values of plot ready for predictions
@@ -48,22 +63,21 @@ i_all_sp <- seq(min(elevational_ranges_species_predicted$elevational_range_midpo
          max(elevational_ranges_species_predicted$elevational_range_midpoint), len=100) #  x-value limits for line
 
 #' Calculate the predicted values from the regression so they can be plotted as a line
-predicted_values_all_sp <- predict(nonlin_reg_quadratic,
-                            data.frame(elevational_range_midpoint=i_all_sp, elevational_range_midpoint2=i_all_sp*i_all_sp)) #  fitted values
-intervals_all_sp <-  predict(nonlin_reg_quadratic,
-                      data.frame(elevational_range_midpoint=i_all_sp, elevational_range_midpoint2=i_all_sp*i_all_sp), interval = "confidence")
+predicted_values_all_sp <- predict(lin_reg,
+                            data.frame(elevational_range_midpoint=i_all_sp)) #  fitted values
+intervals_all_sp <-  predict(lin_reg,
+                      data.frame(elevational_range_midpoint=i_all_sp), interval = "confidence")
 
 #' Put the values into a dataframe
 confidence_bands_all_sp <- data.frame(i_all_sp, intervals_all_sp)
 
 #' Get the coefficients of the equation and put these into text
-cf_all_sp <- signif(coef(nonlin_reg_quadratic), 2)
+cf_all_sp <- signif(coef(lin_reg), 2)
 
 int_term_all_sp <- cf_all_sp[1]
 lin_term_all_sp <- cf_all_sp[2]
-quadratic_term_all_sp <- abs(cf_all_sp[3])
 
-equation_all_sp <- bquote(italic(E[R]) == .(int_term_all_sp) + .(lin_term_all_sp)*italic(E) - .(quadratic_term_all_sp)*italic(E)^2)
+equation_all_sp <- bquote(italic(E[R]) == .(int_term_all_sp) + .(lin_term_all_sp)*italic(E))
 
 #' ## Create the plot
 
