39afca17445ac47061e8e547e6c80a23e5de8026
Jen Thomas <jenny_t152@yahoo.co.uk>
2023-03-08 11:33:18 +0100

    orrect validation to be of schema rather than data file. Add comments.

--- frictionless_data/describe_data_files.py
+++ frictionless_data/describe_data_files.py
@@ -2,9 +2,9 @@ from frictionless import describe, validate
 from pprint import pprint
 
 def describe_data_file(data_file):
-    """Describe a data file using the frictionless schema. Use JSON (rather than YAML).
+    """Describe a data file using the frictionless schema.
 
-    Return the JSON schema."""
+    Return the resource description."""
 
     resource = describe(data_file, type="resource")
     pprint(resource)
@@ -13,17 +13,24 @@ def describe_data_file(data_file):
 
 
 def resource_to_json(resource, json_schema):
+    """Convert the resource description to JSON.
+
+    Output into the specified file."""
 
     resource.to_json(json_schema)
 
 
-def validate_data_file(data_file):
+def validate_data_file(resource_schema):
+    """Validate the data file against the resource schema, checking for errors."""
 
-    report = validate('invalid.csv')
+    report = validate('resource_schema')
     pprint(report.flatten(["rowNumber", "fieldNumber", "type"]))
 
 
 def observations_schema(resource, schema_json_file):
+    """Add further information to the metadata schema.
+
+    Return the schema in JSON format."""
 
     resource.schema.get_field("specimen_label").title = "Specimen label"
     resource.schema.get_field("specimen_label").description = "Unique label given to specimen"
@@ -34,8 +41,8 @@ def observations_schema(resource, schema_json_file):
 def main():
 
     observations_resource = describe_data_file("data/observations.csv")
-    validate_data_file("data/observations.csv")
     observations_schema(observations_resource, "frictionless_data/schema_observations.json")
+    validate_data_file("frictionless_data/schema_observations.json")
 
 
 if __name__ == '__main__':
