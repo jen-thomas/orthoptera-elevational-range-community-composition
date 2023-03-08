875dd18f423d71369782e2348b90240b24b261ca
Jen Thomas <jenny_t152@yahoo.co.uk>
2023-03-08 11:24:21 +0100

    Produce resource for one of data files. First working version.

--- frictionless_data/describe_data_files.py
+++ frictionless_data/describe_data_files.py
@@ -1,15 +1,42 @@
-from frictionless import describe
+from frictionless import describe, validate
+from pprint import pprint
 
-
-def describe_data_file(data_file, schema_file_name):
+def describe_data_file(data_file):
     """Describe a data file using the frictionless schema. Use JSON (rather than YAML).
 
     Return the JSON schema."""
 
-    package = describe(data_file, type="package")
-    json_package = package.to_json()
+    resource = describe(data_file, type="resource")
+    pprint(resource)
 
-    return json_package
+    return resource
 
 
-describe_data_file("../data/observations.csv", "schema_observations.json")
+def resource_to_json(resource, json_schema):
+
+    resource.to_json(json_schema)
+
+
+def validate_data_file(data_file):
+
+    report = validate('invalid.csv')
+    pprint(report.flatten(["rowNumber", "fieldNumber", "type"]))
+
+
+def observations_schema(resource, schema_json_file):
+
+    resource.schema.get_field("specimen_label").title = "Specimen label"
+    resource.schema.get_field("specimen_label").description = "Unique label given to specimen"
+
+    resource_to_json(resource, schema_json_file)
+
+
+def main():
+
+    observations_resource = describe_data_file("data/observations.csv")
+    validate_data_file("data/observations.csv")
+    observations_schema(observations_resource, "frictionless_data/schema_observations.json")
+
+
+if __name__ == '__main__':
+    main()
