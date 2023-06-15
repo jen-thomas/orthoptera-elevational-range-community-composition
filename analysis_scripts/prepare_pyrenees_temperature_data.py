b6327d40805b2f02dcb4ece814109e108d9aaddb
Jen Thomas <jen@falciot.net>
2023-06-15 14:14:22 +0200

    Get data from 2400m and summarise.

--- analysis_scripts/prepare_pyrenees_temperature_data.py
+++ analysis_scripts/prepare_pyrenees_temperature_data.py
@@ -1,12 +1,35 @@
-import csv
+import pandas
 
 
 def get_data(file):
-    """Open the data file."""
+    """Read data from CSV file into a dataframe.
 
-    with open(file, newline='') as csvfile:
-        data = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in data:
-            print(', '.join(row))
+    Return dataframe."""
 
-    return data
+    df = pandas.read_csv(file, header=0)
+
+    return df
+
+
+def get_summary_stats(df):
+    """Get summary statistics about dataframe.
+
+    Return summary stats."""
+
+    df_stats = df['VALOR_LECTURA'].describe()
+
+    return df_stats
+
+
+def main():
+
+    # Get data and summarise for sites at 2400 m
+    data_file_2400 = "../data/xema/temperatures_catalunya_filtered.csv"
+    df_2400 = get_data(data_file_2400)
+
+    df_stats_2400 = get_summary_stats(df_2400)
+    print(df_stats_2400)
+
+
+if __name__ == '__main__':
+    main()
