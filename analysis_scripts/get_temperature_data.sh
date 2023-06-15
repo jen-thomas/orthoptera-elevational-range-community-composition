458a986e7777afb5ac592c7f691dfa35de11ee5d
Jen Thomas <jen@falciot.net>
2023-06-15 13:37:30 +0200

    Make regex stricter. Change file paths.

--- analysis_scripts/get_temperature_data.sh
+++ analysis_scripts/get_temperature_data.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-input_file="../data/xema/temperatures-catalunya_exemple.csv"
-output_file="../data/xema/temperatures-catalunya_filtered_exemple.csv"
+input_file="../dades/temperatures-catalunya.csv"
+output_file="../dades/temperatures_catalunya_filtered.csv"
 
 # Write the header line to the output file
 head -n 1 "$input_file" > "$output_file"
 
 # Select lines with dates from 2021, specific CODI_ESTACIO values and with the CODI_VARIABLE of 32 (temperature).
 # Append lines o the output file.
-grep -E '(^|,)D2|(^|,)ZD' "$input_file" | grep -E ',[0-9]{2}/[0-9]{2}/2021' | grep '.*,.*,32,.*' >> "$output_file"
-
+grep -E '(^|,)ZB|(^|,)Z5' "$input_file" | grep -E ',[0-9]{2}/[0-9]{2}/2021' | grep -E '[A-Z0-9]{14},[A-Z][A-Z0-9]\,32,.*' >> "$output_file"
