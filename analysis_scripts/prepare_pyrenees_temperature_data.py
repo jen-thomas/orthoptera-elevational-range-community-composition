21842cbf36dc13786252cbb1de9b610ec81140b5
Jen Thomas <jen@falciot.net>
2023-06-15 14:29:40 +0200

    Get summary stats for each met station individually.

--- analysis_scripts/prepare_pyrenees_temperature_data.py
+++ analysis_scripts/prepare_pyrenees_temperature_data.py
@@ -21,6 +21,26 @@ def get_summary_stats(df):
     return df_stats
 
 
+def count_rows(df):
+    """Count the number of rows for each value of CODI_ESTACIO.
+
+    Return summary of the number of rows."""
+
+    value_counts = df['CODI_ESTACIO'].value_counts()
+
+    return value_counts
+
+
+def create_distinct_dfs(df, column_name):
+    distinct_values = df[column_name].unique()
+    distinct_dfs = {}
+
+    for value in distinct_values:
+        distinct_dfs[value] = df[df[column_name] == value]
+
+    return distinct_dfs
+
+
 def main():
 
     # Get data and summarise for sites at 2400 m
@@ -29,6 +49,18 @@ def main():
 
     df_stats_2400 = get_summary_stats(df_2400)
     print(df_stats_2400)
+
+    value_counts_2400 = count_rows(df_2400)
+    print(value_counts_2400)
+
+    # Create distinct dataframes for each of the met stations
+    distinct_dfs_estacions_2400 = create_distinct_dfs(df_2400, 'CODI_ESTACIO')
+
+    # Get the summary data for each of these met stations
+    for value, distinct_df in distinct_dfs_estacions_2400.items():
+        print(f"Summary stats for '{value}':")
+        print(get_summary_stats(distinct_df))
+        print("--------")
 
 
 if __name__ == '__main__':
